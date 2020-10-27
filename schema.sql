55f977f8e6fafa7b17a3fa1adb9dc58616f1efe0
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-10-26 20:23:09 -0500

    created the mysql connection

--- schema.sql
+++ schema.sql
@@ -7,18 +7,18 @@ CREATE TABLE employees (
     id INT NOT NULL ,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role-id INT NOT NULL,
-    manager-id INT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL
+    id INT NOT NULL,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE department (
     id INT NOT NULL,
---
+++ server.js
@@ -0,0 +1,13 @@
+var mysql = require("msql");
+var inquirer = require("inquirer");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mysqlpassword",
+    database: "employees_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+})
